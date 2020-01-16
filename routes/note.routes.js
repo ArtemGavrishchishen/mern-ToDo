ef040a3c8194121eb28446882126a0c2fab80b73
Artem Gavrishchishen <artem.gavrishchishen@gmail.com>
2020-01-16 17:59:44 +0200

    add route note update (Server)

--- routes/note.routes.js
+++ routes/note.routes.js
@@ -36,9 +36,8 @@ router.post("/", auth, async (req, res) => {
 
 //=== delete note by id
 router.delete("/", auth, async (req, res) => {
-  const { id } = req.body;
-
   try {
+    const { id } = req.body;
     const note = await Note.findById(id);
     await note.remove();
     res.status(200).json(id);
@@ -48,5 +47,23 @@ router.delete("/", auth, async (req, res) => {
 });
 
 //===   /note/update
+router.put("/", auth, async (req, res) => {
+  try {
+    const {
+      note: { id, title, content }
+    } = req.body;
+    const updateAt = new Date();
+
+    const note = await Note.findOneAndUpdate(
+      { id },
+      { title, content, updateAt },
+      { new: true }
+    );
+
+    res.status(201).json({ note });
+  } catch (e) {
+    res.status(500).json({ message: e });
+  }
+});
 
 module.exports = router;
