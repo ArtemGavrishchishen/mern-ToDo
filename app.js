4ed5ee492d9325d1f528367a4066b21810963dca
Artem Gavrishchishen <artem.gavrishchishen@gmail.com>
2020-01-23 13:10:04 +0200

    deploy

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
-const express = require('express');
-const config = require('config');
-const mongoose = require('mongoose');
+const express = require("express");
+const config = require("config");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use(express.json({ extended: true }));
 
-app.use('/auth', require('./routes/auth.routes'));
-app.use('/note', require('./routes/note.routes'));
-app.use('/user', require('./routes/user.routes'));
+app.use("/auth", require("./routes/auth.routes"));
+app.use("/note", require("./routes/note.routes"));
+app.use("/user", require("./routes/user.routes"));
 
-const PORT = config.get('port') || 5000;
+app.use("/", express.static(path.join(__dirname, "client", "build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
+const PORT = process.env.PORT || config.get("port") || 5000;
 
 async function start() {
   try {
-    await mongoose.connect(config.get('mongoUri'), {
+    await mongoose.connect(config.get("mongoUri"), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true
     });
 
     app.listen(PORT, () =>
-      console.log('Server was started at http://localhost:' + PORT)
+      console.log("Server was started at http://localhost:" + PORT)
     );
   } catch (e) {
-    console.log('Server Error', e.message);
+    console.log("Server Error", e.message);
     process.exit(1);
   }
 }
